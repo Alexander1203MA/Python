6fe892ee6c12b23e8e298b30d53c65704c554d3c
Alexander <ural305@mail.ru>
2024-03-06 19:55:09 +0300

    Функция загрузки

--- notesapp.py
+++ notesapp.py
@@ -5,3 +5,9 @@ import datetime
 def save_notes(notes):
     with open("notes.json", "w") as file:
         json.dump(notes, file)
+
+def load_notes():
+    if not os.path.exists("notes.json"):
+        return []
+    with open("notes.json", "r") as file:
+        return json.load(file)
